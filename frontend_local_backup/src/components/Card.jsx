49dda5fb757353b1bbd250e73ace2a0889e7492a
Jauschua Stout <jauschua.stout@gmail.com>
2025-12-01 09:49:39 -0500

    Stop tracking frontend_local_backup backup directory

--- frontend_local_backup/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Card = ({children}) => {
-  return (
-    <div className={"bg-gradient-to-b from-white-400 from-75% to-indigo-900 to-50% p-6 border-3 rounded-lg shadow-lg"}>{children}</div>
-  )
-}
-
-export default Card
